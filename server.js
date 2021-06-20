0f2af507cce24c818826847a3840e6cf7f198c86
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-20 11:29:54 +0530

    🎉 base server setup

---
+++ server.js
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'hello World!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});

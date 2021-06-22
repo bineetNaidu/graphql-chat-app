52acd0d69e5fcb18bfd999359c0ee7e05dd6fb84
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-22 13:44:00 +0530

    ✨ add Authenticated Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Register from './pages/Register';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import DynamicRoute from './utils/DynamicRoute';
 
 function App() {
   return (
     <Switch>
       <Container className="pt-5">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
+        <DynamicRoute authenticated exact path="/" component={Home} />
+        <DynamicRoute exact path="/register" component={Register} />
+        <DynamicRoute exact path="/login" component={Login} />
       </Container>
     </Switch>
   );

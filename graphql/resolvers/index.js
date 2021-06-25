15a0bc6521f5742389333bc865e7642851fc2652
bineetNaidu <bineet.s.naidu@gmail.com>
2021-06-25 21:21:32 +0530

    🔥 add field resolvers

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,7 +2,23 @@ const { MessageResolvers } = require('./messages');
 const { ReactionResolvers } = require('./reactions');
 const { UserResolvers } = require('./users');
 
+const { User, Message } = require('../../models');
+
 const resolvers = {
+  Message: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+  },
+  Reaction: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+    message: async (parent) => await Message.findByPk(parent.messageId),
+    user: async (parent) =>
+      await User.findByPk(parent.userId, {
+        attributes: ['username', 'imageUrl', 'createdAt'],
+      }),
+  },
+  User: {
+    createdAt: (parent) => parent.createdAt.toISOString(),
+  },
   Query: {
     ...UserResolvers.Query,
     ...MessageResolvers.Query,
